8e42f6e7f4f26a6232886e5b5c92d079b3e71a10
james davies <jamesdavies@jamess-imac.home>
2020-04-20 19:26:33 +0100

    added bootstrap to the main html and created a stylesheet with some enw styles for the main html

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+​
+const render = require("./lib/htmlRenderer");
+
